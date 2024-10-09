6665a7d513e986aa5af2e81deff0f1dfea43cb62
Dasun Kavinda <dasunk@treinetic.com>
2024-10-09 12:06:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
